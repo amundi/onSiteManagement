39d013877526fe7db25642afc5a91a508daf9a56
Fabrice GAZIN <fabrice.gazin@gmail.com>
2020-05-25 22:57:05 +0200

    Changing packaging to jar

--- back/src/main/java/com/amundi/tech/onsite/MainApplication.java
+++ back/src/main/java/com/amundi/tech/onsite/MainApplication.java
@@ -6,12 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MainApplication.class);
-    }
+public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
